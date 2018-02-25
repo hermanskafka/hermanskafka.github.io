3f0dc240d0c597a7489d155723ef750ff83599fd
unknown <sammitra47@gmail.com>
2018-02-25 02:01:23 +0200

    Modify image loading script -> infinite image loading

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,28 +1,22 @@
 /**
  * Created by sam on 2/21/2018.
  */
-var intervalModule =  (function () {
+  (function () {
    var
-       currentPic = 1,
-       state = [],
-        htmlElements = document.getElementsByClassName('line'),
-       elements = elementsToArray(),
-        totalElements = elements.length;
+    currentPic = 1,
+    htmlElements = document.getElementsByClassName('line'),
+    elements = elementsToArray();
 
      function elementsToArray(){
          var elements = [];
-         for(var i =0; i<htmlElements.length; i++)
-             elements.push(htmlElements[i])
+            Array.prototype.forEach.call(htmlElements, el=> elements.push(el))
             return elements
 
      }
-    function addBckImg(){
-             if(checkElements()) clearInterval(interval);
-         var el = getElement();
-             setCss(el);
-             state.unshift(el);
-             resetPicCount();
-             currentPic++
+    function manageBckImg(){
+             setCss(getElement());
+             checkElements();
+             setPicCount();
     }
     function setCss(el){
         el.classList.add('z-index-9');
@@ -31,19 +25,14 @@ var intervalModule =  (function () {
     function getElement(){
         return elements.length % 2 == 0 ?  elements.pop() : elements.shift()
     }
-    function removeCss(){
-        if(state.length < 9) return;
-            var removeElement = state.pop();
-                removeElement.style.removeProperty('background-image')
-                removeElement.classList.remove('z-index-9')
-    }
     function checkElements(){
-          return elements.length == 1;
+          if(elements.length == 0) elements = elementsToArray();
     }
-    function resetPicCount(){
-        if(currentPic == 10) currentPic = 0
+    function setPicCount(){
+        if(currentPic == 10) currentPic = 0;
+        currentPic++;
     }
-    setTimeout(()=>setInterval(addBckImg,100),2000)
+    setTimeout(()=>setInterval(manageBckImg,100),2000)
 
 })()
 
